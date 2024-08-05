346087eddeb927e3eed308870e71503a580558b7
Abdul Rehman <arrehman0r@gmail.com>
2024-08-05 13:38:55 +0500

    chnge src

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,7 @@ class MyDocument extends Document {
               __html: `
                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.url=
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                 })(window,document,'script','dataLayer','GTM-PBWSVMP6');
               `,
@@ -31,7 +31,7 @@ class MyDocument extends Document {
                 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
                 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
                 n.queue=[];t=b.createElement(e);t.async=!0;
-                t.url=v;s=b.getElementsByTagName(e)[0];
+                t.src=v;s=b.getElementsByTagName(e)[0];
                 s.parentNode.insertBefore(t,s)}(window, document,'script',
                 'https://connect.facebook.net/en_US/fbevents.js');
                 fbq('init', '308078109064492');
